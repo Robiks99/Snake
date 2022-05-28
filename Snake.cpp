074cc7b9b64f7857c3964776c17dc06c639437aa
Robiks99 <98769851+Robiks99@users.noreply.github.com>
2022-05-28 15:28:31 +0200

    Added Snake class

---
+++ Snake.cpp
@@ -0,0 +1,175 @@
+#include"Snake.h"
+#define GREED_SIZE 8;
+Snake::Snake(SDL_Window* Window, SDL_Renderer* Renderer){
+    printf("Snake created");
+    window = Window;
+    renderer = Renderer;
+    SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+    rect = new SDL_Rect[snakeLenght];
+    for(int i = 0; i < snakeLenght; i++){
+    rect[i].x = (-8 * i)+64;
+    rect[i].y = 0;
+    rect[i].w = 8;
+    rect[i].h = 8;
+    }
+    moveDirection = EAST;
+    lookDirection = EAST;
+    startTicks = SDL_GetTicks64();
+    moveDelay = 100;
+}
+
+Snake::~Snake(){
+
+}
+
+void Snake::Event(SDL_Event &event){
+    ChangeDirection(event);
+    if(event.key.keysym.sym == SDLK_SPACE){
+        IncreaseSnakeLength();
+    }
+    
+}
+
+void Snake::Update(){
+    Move();
+    BorderCollision();
+}
+
+void Snake::Render(){
+    
+    for(int i = 0; i < snakeLenght-1; i++){
+        SDL_SetRenderDrawColor(renderer, (rect[i].x/4)+128, (rect[i].y/4)+128 , (rect[i].x/4)+128, 255);
+        SDL_RenderFillRect(renderer, &rect[i]);
+    }
+    
+}
+
+void Snake::ChangeDirection(SDL_Event &event){
+    if( event.type == SDL_KEYDOWN)
+    {
+        if(event.key.keysym.sym == SDLK_w){
+            if(lookDirection != SOUTH){
+                moveDirection = NORTH;
+                printf("\nset move direction to NORTH");
+                return;
+            }
+        }
+        if(event.key.keysym.sym == SDLK_s){
+            if(lookDirection != NORTH){
+                moveDirection = SOUTH;
+                printf("\nset move direction to SOUTH");
+                return;
+            }
+        }
+        if(event.key.keysym.sym == SDLK_a){
+            if(lookDirection != EAST){
+                moveDirection = WEST;
+                printf("\nset move direction to WEST");
+                return;
+            }
+        }
+        if(event.key.keysym.sym == SDLK_d){
+            if(lookDirection != WEST){
+                moveDirection = EAST;
+                printf("\nset move direction to EAST");
+                return;
+            }
+        }
+    }
+}
+
+void Snake::Move(){
+    if(moveDelay < SDL_GetTicks64() - startTicks){
+        
+        
+        //Move tail
+        for(int i = snakeLenght-1; i > 0; i--){
+            rect[i].x = rect[i-1].x;
+            rect[i].y = rect[i-1].y;
+        }
+        //Move head
+        if(moveDirection == NORTH){
+            rect[0].y -= GREED_SIZE;
+            lookDirection = NORTH;
+        }
+        if(moveDirection == SOUTH){
+            rect[0].y += GREED_SIZE;
+            lookDirection = SOUTH;
+        }
+        if(moveDirection == WEST){
+            rect[0].x -= GREED_SIZE;
+            lookDirection = WEST;
+        }
+        if(moveDirection == EAST){
+            rect[0].x += GREED_SIZE;
+            lookDirection = EAST;
+        }
+        
+
+        startTicks = SDL_GetTicks64();
+    }
+}
+
+void Snake::BorderCollision(){
+    if(rect[0].x > windowWidth){
+        rect[0].x = 0;
+        return;
+    }
+    if(rect[0].x < 0){
+        rect[0].x = windowWidth - GREED_SIZE;
+        return;
+    }
+    if(rect[0].y > windowHeight){
+        rect[0].y = 0;
+        return;
+    }
+    if(rect[0].y < 0){
+        rect[0].y = windowHeight - GREED_SIZE;
+        return;
+    }
+}
+
+void Snake::SpawnNewSnakeElement(){
+    //przypisz wartość do nowego elementu
+    rect[snakeLenght-1].h = GREED_SIZE;
+    rect[snakeLenght-1].w = GREED_SIZE;
+
+    if(rect[snakeLenght-1].x < rect[snakeLenght-2].x){
+        rect[snakeLenght-1].x = rect[snakeLenght-2].x - GREED_SIZE;
+        rect[snakeLenght-1].y = rect[snakeLenght-2].y;
+    }
+    if(rect[snakeLenght-1].x > rect[snakeLenght-2].x){
+        rect[snakeLenght-1].x = rect[snakeLenght-2].x + GREED_SIZE;
+        rect[snakeLenght-1].y = rect[snakeLenght-2].y;
+    }
+    if(rect[snakeLenght-1].y < rect[snakeLenght-2].y){
+        rect[snakeLenght-1].y = rect[snakeLenght-2].y - GREED_SIZE;
+        rect[snakeLenght-1].x = rect[snakeLenght-2].x;
+    }
+    if(rect[snakeLenght-1].y > rect[snakeLenght-2].y){
+        rect[snakeLenght-1].y = rect[snakeLenght-2].y + GREED_SIZE;
+        rect[snakeLenght-1].x = rect[snakeLenght-2].x;
+    }
+}
+
+void Snake::IncreaseSnakeLength(){
+     //zwiększenie ilości elementów w tablicy
+    //przepisz wartości do nowej tablicy
+    SDL_Rect *tmp = new SDL_Rect[snakeLenght];
+    for(int i = 0; i < snakeLenght; i++){
+        tmp[i] = rect[i];
+    }
+    snakeLenght++;
+    rect = new SDL_Rect[snakeLenght];
+    for(int i = 0; i < snakeLenght - 1; i++){
+        rect[i] = tmp[i];
+    }
+    rect[snakeLenght-1].w = GREED_SIZE;
+    rect[snakeLenght-1].h = GREED_SIZE;
+    //ShowRect();
+}
+
+void Snake::ShowRect(){
+    for(int i = 0; i < snakeLenght-1; i++)
+    printf("iteracja %i: X:%i, Y:%i, W:%i, H:%i\n",i,rect[i].x,rect[i].y,rect[i].w,rect[i].h);
+}
---
+++ Snake.h
@@ -0,0 +1,42 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include<stdio.h>
+#include<SDL2/SDL.h>
+
+class Snake{
+public:
+    Snake(SDL_Window* Window, SDL_Renderer* Renderer);
+    ~Snake();
+
+    void Event(SDL_Event &event);
+    void Update();
+    void Render();
+
+private:
+    void ChangeDirection(SDL_Event &event);
+    void Move();
+    void BorderCollision();
+    void SpawnNewSnakeElement();
+    void IncreaseSnakeLength();
+    void ShowRect();
+
+    int snakeLenght = 15;
+    SDL_Rect *rect;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    int xPos, yPos;
+    int moveDelay;
+    Uint32 startTicks = 0;
+    float dT;
+    int windowHeight, windowWidth;
+    enum MoveDirection{
+        NORTH,
+        EAST,
+        WEST,
+        SOUTH
+    };
+    MoveDirection moveDirection, lookDirection;
+};
+
+#endif
